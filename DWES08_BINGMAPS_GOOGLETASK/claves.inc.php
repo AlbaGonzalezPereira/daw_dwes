d1a3d8408d18e2a8c8d05344f74fa31359ee9196
Alba <98285852+AlbaGonzalezPereira@users.noreply.github.com>
2023-05-21 15:21:16 +0200

    add tarea8

---
+++ DWES08_BINGMAPS_GOOGLETASK/claves.inc.php
@@ -0,0 +1,16 @@
+<?php
+// AÑADE AQUÍ TUS PROPIAS CLAVES (estas claves son falsas)
+//estas llaves se usan para limitar/controlar el uso de las apis
+$keyAEMET = "nopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastempranonopormuchomadrugaramanecemastemprano";
+
+$keyOpenWeatherMap  = '0101010100101001010100101001100';  
+
+//clave bing generada
+$keyBing = "AvnkD-oufg5_jF9ZKSKelKwOchYW1bsjd-V1hynvL5edd-TIcQM-oGAGZUuwu_Qw";  
+
+//claves que me proporcionaste
+$googleClientId     = '891197018098-vrqtebkp7o7mo6idsbdaptg9f9m7ups9.apps.googleusercontent.com'; 
+$googleClientSecret = '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'; 
+
+//incidencias cerca del almacén:
+$incidenciasAlmacen = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/42.38528,-8.68413,42.47415,-8.60239?key=AvnkD-oufg5_jF9ZKSKelKwOchYW1bsjd-V1hynvL5edd-TIcQM-oGAGZUuwu_Qw";
