3666a22c8e1621d136834e817e22662e4f226c0c
Alba <98285852+AlbaGonzalezPereira@users.noreply.github.com>
2023-05-23 19:00:05 +0200

    modificacion

--- DWES_DATOS/claves.inc.php
+++ DWES_DATOS/claves.inc.php
@@ -1,16 +1,16 @@
 <?php
 // AÑADE AQUÍ TUS PROPIAS CLAVES (estas claves son falsas)
 //estas llaves se usan para limitar/controlar el uso de las apis
-$keyAEMET = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBTEJBX0dPTlpBTEVaUEVSRUlSQUBIT1RNQUlMLkNPTSIsImp0aSI6IjkyMzQ5ZjQ5LTIxYjUtNDk2MC1iZjYwLWMxYTZlYmUwZWQwOCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjg0NzY3OTA3LCJ1c2VySWQiOiI5MjM0OWY0OS0yMWI1LTQ5NjAtYmY2MC1jMWE2ZWJlMGVkMDgiLCJyb2xlIjoiIn0.4hiAcqO_YcLe_OZNd9vb-X1wJ3IItiUxG59gXoaq-5s";
+$keyAEMET = "************************************";
 
-$keyOpenWeatherMap  = '6f7dc4624ba4740af695556d6945a5b8';  
+$keyOpenWeatherMap  = '*********************************';  
 
 //clave bing generada
-$keyBing = "AvnkD-oufg5_jF9ZKSKelKwOchYW1bsjd-V1hynvL5edd-TIcQM-oGAGZUuwu_Qw";  
+$keyBing = "**********************************************";  
 
 //claves que me proporcionaste
-$googleClientId     = '891197018098-vrqtebkp7o7mo6idsbdaptg9f9m7ups9.apps.googleusercontent.com'; 
-$googleClientSecret = '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'; 
+$googleClientId     = '**************************'; 
+$googleClientSecret = '**************************'; 
 
 //incidencias cerca del almacén:
 $incidenciasAlmacen = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/42.38528,-8.68413,42.47415,-8.60239?key=AvnkD-oufg5_jF9ZKSKelKwOchYW1bsjd-V1hynvL5edd-TIcQM-oGAGZUuwu_Qw";
